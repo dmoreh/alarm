b1aafc169998c77e46c70eb2ed8b71b77f1d556a
Daniel Moreh <dmoreh@gmail.com>
2015-02-08 04:37:03 -0500

    add alarms and scheduler. doesn't play recordings yet.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var multer  = require('multer');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
 
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
@@ -13,18 +15,27 @@ var recordingSchema = mongoose.Schema({
 	date: Date,
 	played: Boolean
 });
-
 var Recording = mongoose.model('Recording', recordingSchema);
+
+var alarmSchema = mongoose.Schema({
+	dayOfWeek: Number,
+	time: String,
+	enabled: Boolean
+});
+var Alarm = mongoose.model('Alarm', alarmSchema);
 
 var UPLOADS_PATH = './uploads/'
 
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/upload_recording', multer({
 	dest: UPLOADS_PATH,
 	rename: function (fieldname, filename) {
-  		return fieldname + filename + Date.now()
+  		return fieldname + '_' + filename + '_'+ Date.now()
 	}
 }));
 
@@ -48,9 +59,9 @@ app.post('/upload_recording', function(req, res) {
 		date: new Date(),
 		played: false
 	});
-	rec.save(function (err, recording){
+	rec.save(function (err, recording) {
 		if (err) return console.error(err);
-		console.log('saved to db: ' + recording);
+		console.log('recording saved to db: ' + recording);
 	});
 
 	res.redirect('/success');
@@ -61,22 +72,98 @@ app.get('/success', function(req, res) {
 });
 
 app.get('/admin', function(req, res) {
-	res.render('admin', {
-		title: "Admin",
-		classname: "admin",
-		alarms: [
-			{
-				'dayOfWeek': 'Monday',
-				'datetime': new Date()
-			},
-			{'dayOfWeek': 'Tuesday'}
-		]
+	Alarm.find(function (err, alarms) {
+  		if (err) return console.error(err);
+
+		if (!alarms.length) {
+			alarms = [];
+			for (var i = 0; i < 7; i++) {
+				var alarm = {
+					dayOfWeek: i,
+					time: "12:00",
+					enabled: false
+				}
+				alarms.push(alarm)
+			}
+		}
+		res.render('admin', {
+			title: "Admin",
+			classname: "admin",
+			alarms: alarms
+		});
+	});
+});
+
+app.post('/set_alarm', function(req, res) {
+	console.log(req.body)
+
+	var enabledDays = req.body.enabled;
+	if (!enabledDays.constructor === Array) { 
+		enabledDays = [enabledDays];
+	}
+
+	Alarm.remove({}, function(err) {
+		if (err) return console.error(err);
+
+		req.body.times.forEach(function(time, dayOfWeek, arr) {
+			var enabled = enabledDays.indexOf(dayOfWeek.toString()) > -1;
+			console.log(dayOfWeek, time, enabled);
+
+			var alarm = new Alarm({
+				dayOfWeek: dayOfWeek,
+				time: time,
+				enabled: enabled
+			});
+			alarm.save(function (err, alarm) {
+				if (err) return console.error(err);
+				console.log('alarm saved to db: ' + alarm);
+
+				// TODO: figure out sync issues so this can be done at end of loop. 
+				update_schedule();
+			});
+		});
+
+		res.redirect('/success');
 	});
 });
 
 app.get('*', function(req, res) {
 	res.send('bad route dude');
 });
+
+var jobs = []; // TODO: Should jobs be in mongo?
+var update_schedule = function() {
+	// cancel all old jobs
+	jobs.forEach(function(job, idx, arr) {
+		job.cancel();
+	});
+
+	// create new ones for current alarms
+	Alarm.find(function (err, alarms) {
+  		if (err) return console.error(err);
+
+  		for (var i = 0; i < alarms.length; i++) {
+  			alarm = alarms[i]
+  			if (!alarm.enabled) continue;
+
+			var rule = new schedule.RecurrenceRule();
+			rule.dayOfWeek = alarm.dayOfWeek;
+			time = alarm.time.split(":");
+			rule.hour = parseInt(time[0]);
+			rule.minute = parseInt(time[1]);
+
+			var job = schedule.scheduleJob(rule, function() {
+				soundAlarm();
+			});
+			console.log(job);
+			jobs.push(job);
+  		}
+	});
+};
+
+var soundAlarm = function() {
+	console.log("\n\n\n\n\nBEEP BEEP BEEP\n\n\n\n");
+};
 
 var server = app.listen(3000, function() {
 	console.log('Listening on port 3000 yo');
